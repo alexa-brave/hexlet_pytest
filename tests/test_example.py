6baf06b0a6e1944951bb416dda4a473965b6b968
alexa-brave <merkel_795@mail.ru>
2025-11-17 18:04:36 +0300

    break the test

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,7 +2,7 @@ from hexlet_pytest.example import reverse
 
 
 def test_reverse():
-    assert reverse("Hexlet") == "telxeH"
+    assert reverse("Hexlet") == "tlxeH"
 
 
 def test_reverse_for_empty_string():
